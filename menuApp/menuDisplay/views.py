ac2bd4b5c6a0e594c787c8ce55a9eec1360efe4f
Redbeard29 <ben@benshermanmusic.com>
2020-10-17 19:38:22 -0400

    menu display page with 3 additional optional pages created. they are not responsive yet.

--- menuApp/menuDisplay/views.py
+++ menuApp/menuDisplay/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Menu Display')
+    return render(request, 'home.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def locations(request):
+    return render(request, 'locations.html')
+
+def reservations(request):
+    return render(request, 'reservations.html')
