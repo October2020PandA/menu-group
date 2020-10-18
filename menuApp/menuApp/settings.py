0b9c22d42fdc4d8beca778f255be0765bcbdc999
Redbeard29 <ben@benshermanmusic.com>
2020-10-18 18:48:10 -0400

    made menuDisplay pages responsive and changed some fonts. started pos navbar

--- menuApp/menuApp/settings.py
+++ menuApp/menuApp/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'menuApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['pointOfSale/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- menuApp/pointOfSale/views.py
+++ menuApp/pointOfSale/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse('Point of Sale')
+    return render(request, 'pos_main.html')
