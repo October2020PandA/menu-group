4377e0cf1b9d765523fffef73ea7fc6a1ab9f7cb
Andrieu Spong <aspong3@gmail.com>
2020-10-22 09:10:33 -0700

    added some views info

--- menuApp/onlineOrder/urls.py
+++ menuApp/onlineOrder/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('items', views.items),
+    path('cart', views.cart),
+    path('update_item', views.updateItem),
+    path('process_order', views.processOrder),
+    path('charge', views.charge),
     path('checkout', views.checkout, name="checkout"),
-    path('logout', views.logout, name='logout')
+    path('logout', views.logout, name='logout'),
 ]
