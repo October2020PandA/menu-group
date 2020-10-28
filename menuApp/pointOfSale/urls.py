15b4b274371c3a8d4c5d0e00879e55877ea4ade3
Redbeard29 <ben@benshermanmusic.com>
2020-10-27 21:34:18 -0400

    finished backend on menuDisplay, made some progress on backend on POS

--- menuApp/pointOfSale/urls.py
+++ menuApp/pointOfSale/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_to_order', views.add_to_order, name='add_to_order'),
+    path('delete', views.delete, name='delete'),
+    path('checkout', views.checkout, name='checkout'),
 ]
--- menuApp/pointOfSale/views.py
+++ menuApp/pointOfSale/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
+from adminPanel.models import Category, Item
+from pointOfSale.models import *
 
 
 def index(request):
-    return render(request, 'pos_home.html')
+    context = {
+        'order': Order.objects.all(),
+        'items': Item.objects.all(),
+        'categories': Category.objects.all(),
+    }
+    return render(request, 'pos_home.html', context)
+
+def add_to_order(request, item_id):
+    Order.objects.create(
+        
+    )
+
+def delete(request, item_id):
+    pass
+
+def checkout(request):
+    Order.objects.create(
+
+    )
+    return redirect('/')
